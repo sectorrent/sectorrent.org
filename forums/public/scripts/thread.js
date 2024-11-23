d17e77da9c4b7bd1203a79595f93bc94c54da255
DrBrad <brad@bradeagle.com>
2024-11-22 17:16:07 -0700

    Styling and new threads

---
+++ forums/public/scripts/thread.js
@@ -0,0 +1,19 @@
+const categorySelector = document.getElementById('category-selector');
+var currentlySelected = 0;
+
+(function(){
+    categorySelector.value = categorySelector.querySelector('fl-option').getAttribute('value');
+    categorySelector.onchange = function(e){
+        currentlySelected = e.target.value;
+    };
+
+    categorySelector.firstElementChild.onclick = function(e){
+        if(e.target.getAttribute('value') !== categorySelector.value){
+            categorySelector.setAttribute('label', e.target.textContent);
+            categorySelector.value = e.target.getAttribute('value');
+            categorySelector.selectedIndex = e.target.index;
+            categorySelector.parentElement.firstElementChild.value = categorySelector.value;
+            categorySelector.dispatchEvent(new Event('change'));
+        }
+    };
+}());
