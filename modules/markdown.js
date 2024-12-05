31cd7d853348621806350d63ef89e370e5dea73f
DrBrad <brad@bradeagle.com>
2024-12-04 22:14:51 -0700

    Copy button change

--- modules/markdown.js
+++ modules/markdown.js
@@ -34,7 +34,7 @@ exports.parse = (markdown) => {
 
                     codeLanguage = (line.slice(3) == '') ? 'plain' : line.slice(3).toLowerCase();
                     const previousLine = lines[i+1];
-                    codeLines.push(`<code-header>${codeLanguage}<copy></copy></code-header>`);
+                    codeLines.push(`<code-header>${codeLanguage}<button class='copy' type='button' onclick='onCopy(this.parentElement)'><svg viewBox='0 0 24 24'><path d='M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z' /></svg></button></code-header>`);
                     codeLines.push(`<pre language='${codeLanguage}'><code>${tokenizeLine(codeLanguage, previousLine)}</code>`);
                     i += 2;
                     continue;
