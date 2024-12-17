01d99413ca4f20c101b1807b4e556a9f2c896c3a
DrBrad <brad@bradeagle.com>
2024-12-16 18:17:30 -0700

    Create comment for thread

--- forum/public/scripts/form.js
+++ forum/public/scripts/form.js
@@ -31,58 +31,6 @@ async function solveChallenge(challenge, difficulty = 4){
     }
 }
 
-function onSubmit(event){
-    event.preventDefault();
-    if(processing && !solved){
-        return;
-    }
-    processing = true;
-
-    const formData = formToJSON(event.target);
-    formData.pow = pow;
-
-    fetch(event.target.getAttribute('action'), {
-        method: event.target.getAttribute('method'),
-        credentials: 'include',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData)
-    
-    }).then(response => {
-        if(!response.ok){
-            throw new Error('Failed to get data');
-        }
-
-        return response.json();
-
-    }).then((data) => {
-        switch(data.status){
-            case 200:
-                alert(data.data.message, data.data.link);
-                return;
-
-            case 417:
-                processing = false;
-                if(data.data.fields){
-                    for(const key of data.data.fields){
-                        document.querySelector("input[name='"+key.type+"']").parentElement.setAttribute('error', key.message);
-                    }
-                }
-
-            default:
-                processing = false;
-                document.querySelector('response').textContent = data.data.message;
-                break;
-        }
-
-    }).catch(function(error){
-        console.log(error);
-        document.querySelector('response').textContent = error;
-        processing = false;
-    });
-}
-
 function alert(message, link){
     const alert = document.querySelector('alert');
     alert.innerHTML = message;
--- forum/public/scripts/thread.js
+++ forum/public/scripts/thread.js
@@ -4,6 +4,7 @@ const pinThread = document.querySelector('button[action="pin-thread"]');
 const unpinThread = document.querySelector('button[action="unpin-thread"]');
 const deleteThread = document.querySelector('button[action="delete-thread"]');
 const reportThread = document.querySelector('button[action="report-thread"]');
+const comments = document.querySelector('comments');
 
 var loading = false;
 
@@ -297,3 +298,98 @@ function onreportcomment(e){
         loading = false;
     });
 };
+
+function onSubmit(event){
+    event.preventDefault();
+    if(processing && !solved){
+        return;
+    }
+    processing = true;
+
+    const formData = formToJSON(event.target);
+    formData.pow = pow;
+
+    fetch(event.target.getAttribute('action'), {
+        method: event.target.getAttribute('method'),
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData)
+    
+    }).then(response => {
+        if(!response.ok){
+            throw new Error('Failed to get data');
+        }
+
+        return response.json();
+
+    }).then((data) => {
+        switch(data.status){
+            case 200:
+                comments.appendChild(createComment(data));
+                return;
+
+            default:
+                processing = false;
+                document.querySelector('response').textContent = data.data.message;
+                break;
+        }
+
+    }).catch(function(error){
+        console.log(error);
+        document.querySelector('response').textContent = error;
+        processing = false;
+    });
+}
+
+function createComment(data){
+    const comment = document.createElement('comment');
+
+    const userIconLink = document.createElement('a');
+    userIconLink.className = 'user-icon';
+    userIconLink.href = `/u/${data.user.username}`;
+
+    const userIcon = document.createElement('img');
+    userIcon.src = data.user.avatar;
+    userIcon.onload = function(e){
+        this.className = 'show';
+    };
+    userIcon.title = data.user.username;
+    userIcon.draggable = false;
+    userIcon.oncontextmenu = function(e){
+        return false;
+    };
+
+    userIconLink.appendChild(userIcon);
+    comment.appendChild(userIconLink);
+
+    const commentInner = document.createElement('comment-inner');
+    const commentHeader = document.createElement('comment-header');
+
+    const userLink = document.createElement('a');
+    userLink.href = `/u/${data.user.username}`;
+
+    if(data.user.role > 1){
+        //CREATE MODERATOR ICON
+    }
+
+    userLink.appendChild(document.createTextNode(data.user.username));
+    commentHeader.appendChild(userLink);
+
+    const date = document.createElement('date');
+    //date.appendChild(document.createTextNode(data.user.username));
+    commentHeader.appendChild(date);
+
+    //OPTIONS APPEND...
+
+    commentInner.appendChild(commentHeader);
+
+    const preview = document.createElement('preview');
+    preview.innerHTML = markdownToHtml(data.content);
+
+    commentInner.appendChild(preview);
+    comment.appendChild(commentInner);
+
+    return comment;
+}
