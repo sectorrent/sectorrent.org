243d6a106dee87b8a11372d4789ccb2386414ede
DrBrad <brad@bradeagle.com>
2024-12-26 01:13:04 -0700

    Fix to submit

--- forum/public/scripts/form.js
+++ forum/public/scripts/form.js
@@ -85,3 +85,67 @@ function formToJSON(form){
     }
     return json;
 }
+
+function onsubmit(event){
+    event.preventDefault();
+    if(processing || !solved){
+        return;
+    }
+    processing = true;
+
+    const formData = formToJSON(event.target);
+    formData.pow = pow;
+
+    fetch(event.target.getAttribute('action'), {
+        method: event.target.getAttribute('method'),
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData)
+    
+    }).then(response => {
+        if(!response.ok){
+            throw new Error('Failed to get data');
+        }
+
+        return response.json();
+
+    }).then((data) => {
+        switch(data.status){
+            case 200:
+                alert(data.data.message, data.data.link);
+                return;
+
+            case 400:
+                processing = false;
+                document.querySelector('pow svg path').setAttribute('d', 'M18 12A6.41 6.41 0 0 1 20.87 12.67A11.63 11.63 0 0 0 21 11V5L12 1L3 5V11C3 16.55 6.84 21.74 12 23C12.35 22.91 12.7 22.8 13 22.68A6.42 6.42 0 0 1 11.5 18.5A6.5 6.5 0 0 1 18 12M18 14.5V13L15.75 15.25L18 17.5V16A2.5 2.5 0 0 1 20.24 19.62L21.33 20.71A4 4 0 0 0 18 14.5M18 21A2.5 2.5 0 0 1 15.76 17.38L14.67 16.29A4 4 0 0 0 18 22.5V24L20.25 21.75L18 19.5Z');
+                document.querySelector('pow span').textContent = 'PoW verification in progress.';
+                document.querySelector('response').textContent = data.data.message;
+                pow = data.data.pow;
+                solveChallenge(pow.challenge, pow.difficulty);
+
+            case 417:
+                processing = false;
+                if(data.data.fields){
+                    for(const key of data.data.fields){
+                        document.querySelector("input[name='"+key.type+"']").parentElement.setAttribute('error', key.message);
+                    }
+                }
+                document.querySelector('pow svg path').setAttribute('d', 'M18 12A6.41 6.41 0 0 1 20.87 12.67A11.63 11.63 0 0 0 21 11V5L12 1L3 5V11C3 16.55 6.84 21.74 12 23C12.35 22.91 12.7 22.8 13 22.68A6.42 6.42 0 0 1 11.5 18.5A6.5 6.5 0 0 1 18 12M18 14.5V13L15.75 15.25L18 17.5V16A2.5 2.5 0 0 1 20.24 19.62L21.33 20.71A4 4 0 0 0 18 14.5M18 21A2.5 2.5 0 0 1 15.76 17.38L14.67 16.29A4 4 0 0 0 18 22.5V24L20.25 21.75L18 19.5Z');
+                document.querySelector('pow span').textContent = 'PoW verification in progress.';
+                pow = data.data.pow;
+                solveChallenge(pow.challenge, pow.difficulty);
+
+            default:
+                processing = false;
+                document.querySelector('response').textContent = data.data.message;
+                break;
+        }
+
+    }).catch(function(error){
+        console.log(error);
+        document.querySelector('response').textContent = error;
+        processing = false;
+    });
+}
