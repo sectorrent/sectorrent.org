80a4ac504350ff8e0a9a151a23b4a28d0976778b
DrBrad <brad@bradeagle.com>
2024-12-28 01:17:51 -0700

    Archive patches

--- forum/public/scripts/thread.js
+++ forum/public/scripts/thread.js
@@ -58,12 +58,14 @@ if(archiveThread){
                     archiveThread.setAttribute('method', 'DELETE');
                     archiveThread.querySelector('svg path').setAttribute('d', 'M20.55,5.22l-1.39-1.68C18.88,3.21,18.47,3,18,3H6C5.53,3,5.12,3.21,4.85,3.55L3.46,5.22C3.17,5.57,3,6.01,3,6.5V19 c0,1.1,0.89,2,2,2h14c1.1,0,2-0.9,2-2V6.5C21,6.01,20.83,5.57,20.55,5.22z M12,9.5l5.5,5.5H14v2h-4v-2H6.5L12,9.5z M5.12,5 l0.82-1h12l0.93,1H5.12z');
                     archiveThread.querySelector('span').textContent = 'Unarchive thread';
+                    document.querySelector('section').classList.add('archived');
                     break;
 
                 case 'DELETE':
                     archiveThread.setAttribute('method', 'PUT');
                     archiveThread.querySelector('svg path').setAttribute('d', 'M20.54 5.23l-1.39-1.68C18.88 3.21 18.47 3 18 3H6c-.47 0-.88.21-1.16.55L3.46 5.23C3.17 5.57 3 6.02 3 6.5V19c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V6.5c0-.48-.17-.93-.46-1.27zM12 17.5L6.5 12H10v-2h4v2h3.5L12 17.5zM5.12 5l.81-1h12l.94 1H5.12z');
                     archiveThread.querySelector('span').textContent = 'Archive thread';
+                    document.querySelector('section').classList.remove('archived');
                     break;
             }
 
