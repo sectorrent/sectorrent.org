2080f25a90186888c320049030de2193862025b5
DrBrad <brad@bradeagle.com>
2025-01-07 13:45:56 -0700

    Bug fixes

--- forum/public/scripts/form.js
+++ forum/public/scripts/form.js
@@ -20,6 +20,8 @@ async function solveChallenge(challenge, difficulty = 4){
 
         if(hashHex.startsWith('0'.repeat(difficulty))){
             pow.nonce = nonce;
+            document.querySelector('input[type="submit"][disabled]').disabled = false;
+            document.querySelector('label[type="submit"][disabled]').removeAttribute('disabled');
             document.querySelector('pow svg path').setAttribute('d', 'M10,17L6,13L7.41,11.59L10,14.17L16.59,7.58L18,9M12,1L3,5V11C3,16.55 6.84,21.74 12,23C17.16,21.74 21,16.55 21,11V5L12,1Z');
             document.querySelector('pow span').textContent = 'PoW verification complete.';
             solved = true;
@@ -116,6 +118,8 @@ function onsubmit(event){
 
             case 400:
                 processing = false;
+                document.querySelector('input[type="submit"]').disabled = true;
+                document.querySelector('label[type="submit"]').setAttribute('disabled', 'true');
                 document.querySelector('pow svg path').setAttribute('d', 'M18 12A6.41 6.41 0 0 1 20.87 12.67A11.63 11.63 0 0 0 21 11V5L12 1L3 5V11C3 16.55 6.84 21.74 12 23C12.35 22.91 12.7 22.8 13 22.68A6.42 6.42 0 0 1 11.5 18.5A6.5 6.5 0 0 1 18 12M18 14.5V13L15.75 15.25L18 17.5V16A2.5 2.5 0 0 1 20.24 19.62L21.33 20.71A4 4 0 0 0 18 14.5M18 21A2.5 2.5 0 0 1 15.76 17.38L14.67 16.29A4 4 0 0 0 18 22.5V24L20.25 21.75L18 19.5Z');
                 document.querySelector('pow span').textContent = 'PoW verification in progress.';
                 document.querySelector('response').textContent = data.status_message;
@@ -129,6 +133,8 @@ function onsubmit(event){
                         document.querySelector("input[name='"+key.type+"']").parentElement.setAttribute('error', key.message);
                     }
                 }
+                document.querySelector('input[type="submit"]').disabled = true;
+                document.querySelector('label[type="submit"]').setAttribute('disabled', 'true');
                 document.querySelector('pow svg path').setAttribute('d', 'M18 12A6.41 6.41 0 0 1 20.87 12.67A11.63 11.63 0 0 0 21 11V5L12 1L3 5V11C3 16.55 6.84 21.74 12 23C12.35 22.91 12.7 22.8 13 22.68A6.42 6.42 0 0 1 11.5 18.5A6.5 6.5 0 0 1 18 12M18 14.5V13L15.75 15.25L18 17.5V16A2.5 2.5 0 0 1 20.24 19.62L21.33 20.71A4 4 0 0 0 18 14.5M18 21A2.5 2.5 0 0 1 15.76 17.38L14.67 16.29A4 4 0 0 0 18 22.5V24L20.25 21.75L18 19.5Z');
                 document.querySelector('pow span').textContent = 'PoW verification in progress.';
                 pow = data.data.pow;
